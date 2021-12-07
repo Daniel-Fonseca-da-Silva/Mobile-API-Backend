5e4ab55a3887c9bfa3b1044abb1890f12273a71f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-07 08:50:59 -0300

    Created model purchase

---
+++ src/models/purchases/purchase.entity.ts
@@ -0,0 +1,10 @@
+export class Purchase {
+  client_id: string;
+  client_name: string;
+  total_to_pay: number;
+  card_number: string;
+  value: number;
+  cvv: number;
+  card_holder_name: string;
+  exp_date: string;
+}
