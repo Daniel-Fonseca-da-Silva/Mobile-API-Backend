b2be5c69e59dfd8cb0d99d388ec0ff80ae79aa10
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-07 09:05:57 -0300

    Implemented service purchases

--- src/models/purchases/purchases.service.ts
+++ src/models/purchases/purchases.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Purchase } from './purchase.entity';
 
 @Injectable()
-export class PurchasesService {}
+export class PurchasesService {
+  private purchases: Purchase[] = [
+    {
+      client_id: '7e655c6e-e8e5-4349-8348-e51e0ff3072e',
+      client_name: 'Luke Skywalker',
+      total_to_pay: 1236,
+      card_number: '1234123412341234',
+      value: 7990,
+      cvv: 789,
+      card_holder_name: 'Luke Skywalker',
+      exp_date: '12/24',
+    },
+  ];
+
+  findAllPurchases() {
+    if (!this.purchases.length) {
+      throw new HttpException(
+        `This purchase don't exist more`,
+        HttpStatus.NO_CONTENT,
+      );
+    }
+    return this.purchases;
+  }
+
+  findOnePurchase(id: string) {
+    const purchasesSaved = this.purchases.find(
+      (purchases: Purchase) => purchases.client_id === id,
+    );
+    if (!purchasesSaved) {
+      throw new HttpException(
+        `Don't possible to find this purchase with id ${id}`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return purchasesSaved;
+  }
+  createProduct(createPurchaseDTO: any) {
+    this.purchases.push(createPurchaseDTO);
+  }
+  updateProduct(id: string, updatePurchaseDTO: any) {
+    const indexpurchase = this.purchases.findIndex(
+      (purchases) => purchases.client_id === id,
+    );
+
+    this.purchases[indexpurchase] = updatePurchaseDTO;
+  }
+  removeProduct(id: string) {
+    const indexpurchase = this.purchases.findIndex(
+      (purchases) => purchases.client_id === id,
+    );
+    if (indexpurchase != 0) {
+      this.purchases.splice(indexpurchase, 1);
+    }
+  }
+}
