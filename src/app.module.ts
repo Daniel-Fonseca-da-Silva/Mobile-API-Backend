3b49e281d7200fd3c801ada2b839fecfb6683f9a
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-07 08:40:55 -0300

    Created service purchases

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { ClientsModule } from './modules/clients/clients.module';
 import { HistoriesModule } from './modules/histories/histories.module';
 import { CreditCardsModule } from './modules/credit-cards/credit-cards.module';
 import { PurchasesController } from './controllers/purchases/purchases.controller';
+import { PurchasesService } from './models/purchases/purchases.service';
 
 @Module({
   imports: [ProductsModule, ClientsModule, HistoriesModule, CreditCardsModule],
   controllers: [PurchasesController],
-  providers: [],
+  providers: [PurchasesService],
 })
 export class AppModule {}
