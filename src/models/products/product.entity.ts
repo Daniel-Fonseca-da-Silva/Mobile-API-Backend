a1eb2d384679dc7a6aeb663bcc63a294ff2d0ba9
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-08 11:31:06 -0300

    Created entity products inside database

--- src/models/products/product.entity.ts
+++ src/models/products/product.entity.ts
@@ -1,9 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'products' })
 export class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: number;
+
+  @Column({ length: 60 })
   title: string;
+
+  @Column({ nullable: true })
   price: number;
+
+  @Column({ length: 9 })
   zipcode: string;
+
+  @Column({ length: 60 })
   seller: string;
+
+  @Column({ name: 'thumbnail_hd', length: 255 })
   thumbnailHd: string;
+
+  @Column({ name: 'date_at', length: 15 })
   date: string;
 }
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from 'src/controllers/products/products.controller';
 import { ProductsService } from 'src/models/products/products.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/models/products/product.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
