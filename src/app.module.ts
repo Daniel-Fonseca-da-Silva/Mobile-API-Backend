b61af03cefa4d6f3464f5a4f03d23f3769afa81b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-08 11:28:28 -0300

    Created environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ClientsModule } from './modules/clients/clients.module';
 import { HistoriesModule } from './modules/histories/histories.module';
 import { CreditCardsModule } from './modules/credit-cards/credit-cards.module';
 import { PurchasesModule } from './modules/purchases/purchases.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -12,6 +14,21 @@ import { PurchasesModule } from './modules/purchases/purchases.module';
     HistoriesModule,
     CreditCardsModule,
     PurchasesModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 5432)),
+        username: configService.get('DB_USERNAME', 'api'),
+        password: configService.get('DB_PASSWORD', 'api123456'),
+        database: configService.get('DB_DATABASE', 'api'),
+        entities: [__dirname + '/**/*entity{.js,.ts}'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
   ],
   controllers: [],
   providers: [],
