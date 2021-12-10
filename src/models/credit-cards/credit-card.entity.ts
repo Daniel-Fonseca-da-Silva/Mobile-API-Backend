b219b1189bab73dabb3910a1673ffb4b95b52ac9
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-10 08:36:22 -0300

    Mapped entity credit car

--- src/models/credit-cards/credit-card.entity.ts
+++ src/models/credit-cards/credit-card.entity.ts
@@ -1,7 +1,21 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+@Entity({ name: 'credit_cards' })
 export class CreditCard {
+  @PrimaryGeneratedColumn('uuid')
+  card_id: string;
+
+  @Column({ type: 'varchar', length: 16, unique: true })
   card_number: string;
+
+  @Column({ type: 'varchar', length: 20 })
   card_holder_name: string;
+
+  @Column({ type: 'int', width: 11 })
   value: number;
+
+  @Column({ type: 'int', width: 3 })
   cvv: number;
+
+  @Column({ type: 'varchar', length: 5 })
   exp_date: string;
 }
